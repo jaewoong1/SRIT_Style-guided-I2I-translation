dc19364d08076bb2cb935fcf2852adb6800cd339
Jay C <54341727+jaewoong1@users.noreply.github.com>
2021-12-09 10:51:22 +0900

    Update solver.py

--- core/solver.py
+++ core/solver.py
@@ -193,24 +193,22 @@ def compute_d_loss(nets, args, x_real, y_org, y_trg, z_trg=None, x_ref=None, mas
     out2 = nets.discriminator(x_fake, y_trg)
 
     if args.SGD == 1:
-        count = 0
         r1 = r2 = r3 = r4 = 0
         if z_trg is not None:
             out = nets.discriminator(x_real, y_org)
             loss_real = adv_loss(out, 1)
             loss_fake = adv_loss(out2, 0)
         else:
-            out = nets.discriminator(x_ref, y_trg)
-            out3 = nets.discriminator(x_real, y_org)
+            out3 = nets.discriminator(x_ref, y_trg)
             for i in range(args.batch_size):
-                count += 1
-                r1 += adv_loss(out[i:i+1]-out2[i:i+1], 1)
-                r3 += adv_loss(out3[i:i + 1] - out2[i:i + 1], 1)
-                r2 += adv_loss(out2[i:i+1]-out[i:i+1], 0)
-                r4 += adv_loss(out2[i:i + 1] - out3[i:i + 1], 0)
+                r1 += adv_loss(out3[i:i+1]-out2[i:i+1], 1)
+                r3 += adv_loss(out[i:i + 1] - out2[i:i + 1], 1)
+                r2 += adv_loss(out2[i:i+1]-out3[i:i+1], 0)
+                r4 += adv_loss(out2[i:i + 1] - out[i:i + 1], 0)
 
-            r1 /= count
-            r2 /= count
+            r1 /= args.batch_size
+            r2 /= args.batch_size
+            
             if r1 != 0:
                 loss_real = r1 + r3
                 loss_fake = r2 + r4
